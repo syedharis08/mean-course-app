2c99582498e626267a5dc9777bfdaa09310d299d
syedharis08 <syedharis14@gmail.com>
2022-03-08 15:01:05 +0500

    initial commit with outputting posts implementation

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.components.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'No Content';
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+}
