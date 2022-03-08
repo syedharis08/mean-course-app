61f114cee9f142ba33bbfcce7650e128f15e3792
syedharis08 <syedharis14@gmail.com>
2022-03-08 16:44:08 +0500

    added post model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Post } from './posts/post-create/post.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  storedPosts = [];
+  storedPosts: Post[] = [];
 
   onPostAdded(post) {
     this.storedPosts.push(post);
