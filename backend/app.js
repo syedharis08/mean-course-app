5595d130341ed5e2f56e246b2f325fefa7806180
syedharis08 <syedharis14@gmail.com>
2022-03-13 00:33:42 +0500

    fetch inital post

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.send("Hello from express");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "asdasfadf233e",
+      title: "First Post",
+      content: "This is coming from server",
+    },
+    {
+      id: "asd234rewsdg23ew",
+      title: "Second Post",
+      content: "This is coming from server",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully",
+    posts: posts,
+  });
 });
 
 module.exports = app;
